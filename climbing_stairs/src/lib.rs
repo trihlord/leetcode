c43c3414866fc089a4d753229544e27f225db544
Vladimir Kubantsev <trihlord@yandex.ru>
2025-01-28 23:13:38 +0300

    added case 3 and 4

--- climbing_stairs/src/lib.rs
+++ climbing_stairs/src/lib.rs
@@ -1,6 +1,4 @@
-/// <https://leetcode.com/problems/climbing-stairs>
-/// 
-/// # 70. Climbing Stairs
+/// # [70. Climbing Stairs](https://leetcode.com/problems/climbing-stairs)
 ///
 /// You are climbing a staircase. It takes n steps to reach the top.
 ///
@@ -9,20 +7,20 @@
 /// ## Example 1
 ///
 /// **Input:** n = 2
-/// 
+///
 /// **Output:** 2
-/// 
+///
 /// **Explanation:** There are two ways to climb to the top.
-/// 
+///
 /// 1. 1 step + 1 step
 /// 2. 2 steps
 ///
 /// ## Example 2
 ///
 /// **Input:** n = 3
-/// 
+///
 /// **Output:** 3
-/// 
+///
 /// **Explanation:** There are three ways to climb to the top.
 ///
 /// 1. 1 step + 1 step + 1 step
@@ -53,11 +51,11 @@ mod tests {
     /// ## Case 3
     ///
     /// **Input:** n = 4
-    /// 
+    ///
     /// **Output:** 5
-    /// 
+    ///
     /// **Explanation:** There are five ways to climb to the top.
-    /// 
+    ///
     /// 1. 1 step + 1 step + 1 step + 1 step
     /// 2. 1 step + 1 step + 2 steps
     /// 3. 1 step + 2 steps + 1 step
@@ -66,5 +64,26 @@ mod tests {
     #[test]
     fn case_3_works() {
         assert_eq!(climb_stairs(4), 5);
+    }
+
+    /// ## Case 4
+    ///
+    /// **Input:** n = 5
+    ///
+    /// **Output:** 8
+    ///
+    /// **Explanation:** There are eight ways to climb to the top.
+    ///
+    /// 1. 1 step + 1 step + 1 step + 1 step + 1 step
+    /// 2. 1 step + 1 step + 1 step + 2 steps
+    /// 3. 1 step + 1 step + 2 steps + 1 step
+    /// 4. 1 step + 2 steps + 1 step + 1 step
+    /// 5. 2 steps + 1 step + 1 step + 1 step
+    /// 6. 1 step + 2 steps + 2 steps
+    /// 7. 2 steps + 1 step + 2 steps
+    /// 8. 2 steps + 2 steps + 1 step
+    #[test]
+    fn case_4_works() {
+        assert_eq!(climb_stairs(5), 8);
     }
 }
