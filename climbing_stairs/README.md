a3088a47529e70b99215c662ec06072fddf9e97a
Vladimir Kubantsev <trihlord@yandex.ru>
2025-03-03 22:14:21 +0300

    climbing stairs solution

---
+++ climbing_stairs/README.md
@@ -0,0 +1,7 @@
+# climb_stairs
+
+Test time limit.
+
+```shell
+RUST_TEST_TIME_UNIT=500,2000 cargo +nightly test tests::climbs_forty_five_stairs -- -Z unstable-options --ensure-time
+```
--- climbing_stairs/src/lib.rs
+++ climbing_stairs/src/lib.rs
@@ -1,40 +1,19 @@
-/// # [70. Climbing Stairs](https://leetcode.com/problems/climbing-stairs)
-///
-/// You are climbing a staircase. It takes n steps to reach the top.
-///
-/// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
-///
-/// ## Example 1
-///
-/// **Input:** n = 2
-///
-/// **Output:** 2
-///
-/// **Explanation:** There are two ways to climb to the top.
-///
-/// 1. 1 step + 1 step
-/// 2. 2 steps
-///
-/// ## Example 2
-///
-/// **Input:** n = 3
-///
-/// **Output:** 3
-///
-/// **Explanation:** There are three ways to climb to the top.
-///
-/// 1. 1 step + 1 step + 1 step
-/// 2. 1 step + 2 steps
-/// 3. 2 steps + 1 step
-///
-/// ## Constraints:
-///
-/// - `1 <= n <= 45`
-pub fn climb_stairs(n: i32) -> i32 {
-    match n {
-        1..=2 => n,
-        3..=45 => climb_stairs(n - 2) + climb_stairs(n - 1),
-        _ => panic!("n must be in range [1, 45]"),
+pub struct Solution;
+
+impl Solution {
+    pub fn climb_stairs(n: i32) -> i32 {
+        match n {
+            1..=45 => {
+                let mut left = 0;
+                let mut right = 1;
+                for _ in 1..=n {
+                    right = left + right;
+                    left = right - left;
+                }
+                right
+            }
+            _ => panic!("n must be in range from 1 to 45"),
+        }
     }
 }
 
@@ -43,57 +22,32 @@ mod tests {
     use super::*;
 
     #[test]
-    fn example_1_works() {
-        assert_eq!(climb_stairs(2), 2);
+    fn climbs_one_stair() {
+        assert_eq!(1, Solution::climb_stairs(1));
     }
 
     #[test]
-    fn example_2_works() {
-        assert_eq!(climb_stairs(3), 3);
-    }
-
-    /// ## Case 3
-    ///
-    /// **Input:** n = 4
-    ///
-    /// **Output:** 5
-    ///
-    /// **Explanation:** There are five ways to climb to the top.
-    ///
-    /// 1. 1 step + 1 step + 1 step + 1 step
-    /// 2. 1 step + 1 step + 2 steps
-    /// 3. 1 step + 2 steps + 1 step
-    /// 4. 2 steps + 1 step + 1 step
-    /// 5. 2 steps + 2 steps
-    #[test]
-    fn case_3_works() {
-        assert_eq!(climb_stairs(4), 5);
-    }
-
-    /// ## Case 4
-    ///
-    /// **Input:** n = 5
-    ///
-    /// **Output:** 8
-    ///
-    /// **Explanation:** There are eight ways to climb to the top.
-    ///
-    /// 1. 1 step + 1 step + 1 step + 1 step + 1 step
-    /// 2. 1 step + 1 step + 1 step + 2 steps
-    /// 3. 1 step + 1 step + 2 steps + 1 step
-    /// 4. 1 step + 2 steps + 1 step + 1 step
-    /// 5. 2 steps + 1 step + 1 step + 1 step
-    /// 6. 1 step + 2 steps + 2 steps
-    /// 7. 2 steps + 1 step + 2 steps
-    /// 8. 2 steps + 2 steps + 1 step
-    #[test]
-    fn case_4_works() {
-        assert_eq!(climb_stairs(5), 8);
+    fn climbs_two_stairs() {
+        assert_eq!(2, Solution::climb_stairs(2));
     }
 
     #[test]
-    #[ignore = "time limit exceeded"]
-    fn case_31_works() {
-        assert_eq!(climb_stairs(45), 1836311903);
+    fn climbs_three_stairs() {
+        assert_eq!(3, Solution::climb_stairs(3));
+    }
+
+    #[test]
+    fn climbs_four_stairs() {
+        assert_eq!(5, Solution::climb_stairs(4));
+    }
+
+    #[test]
+    fn climbs_five_stairs() {
+        assert_eq!(8, Solution::climb_stairs(5));
+    }
+
+    #[test]
+    fn climbs_forty_five_stairs() {
+        assert_eq!(1836311903, Solution::climb_stairs(45));
     }
 }
