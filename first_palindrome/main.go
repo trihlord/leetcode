4ac23f0a616f05e5cd509f04cdef35eb2dc49a31
Vladimir Kubantsev <trihlord@gmail.com>
2025-08-11 21:23:34 +0300

    chore(first_polindrome): fix palindrome misspell

--- first_palindrome/main.go
+++ first_palindrome/main.go
@@ -2,14 +2,14 @@ package first_palindrome
 
 func FirstPalindrome(words []string) string {
 	for _, word := range words {
-		if isPolindrome(word) {
+		if isPalindrome(word) {
 			return word
 		}
 	}
 	return ""
 }
 
-func isPolindrome(word string) bool {
+func isPalindrome(word string) bool {
 	for i, j := 0, len(word)-1; i < j; i, j = i+1, j-1 {
 		if word[i] != word[j] {
 			return false
