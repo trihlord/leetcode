54c1bb52134affccfe783b6481b641e945f614a4
Vladimir Kubantsev <trihlord@gmail.com>
2025-08-11 21:20:33 +0300

    feat(first_polindrome): successfull short  solution

--- first_palindrome/main.go
+++ first_palindrome/main.go
@@ -2,10 +2,9 @@ package first_palindrome
 
 func FirstPalindrome(words []string) string {
 	for _, word := range words {
-		rr := []rune(word)
 		p := true
-		for i, j := 0, len(rr)-1; i < j; i, j = i+1, j-1 {
-			if rr[i] != rr[j] {
+		for i, j := 0, len(word)-1; i < j; i, j = i+1, j-1 {
+			if word[i] != word[j] {
 				p = false
 				break
 			}
