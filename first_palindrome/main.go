36aadf32e148577de5e8b2b3bd4d7902412a1857
Vladimir Kubantsev <trihlord@gmail.com>
2025-08-11 21:22:05 +0300

    feat(first_polindrome): successfull refactored solution

--- first_palindrome/main.go
+++ first_palindrome/main.go
@@ -2,16 +2,18 @@ package first_palindrome
 
 func FirstPalindrome(words []string) string {
 	for _, word := range words {
-		p := true
-		for i, j := 0, len(word)-1; i < j; i, j = i+1, j-1 {
-			if word[i] != word[j] {
-				p = false
-				break
-			}
-		}
-		if p {
+		if isPolindrome(word) {
 			return word
 		}
 	}
 	return ""
+}
+
+func isPolindrome(word string) bool {
+	for i, j := 0, len(word)-1; i < j; i, j = i+1, j-1 {
+		if word[i] != word[j] {
+			return false
+		}
+	}
+	return true
 }
