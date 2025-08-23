59993f1fd6884f98a2fbec8a806e5b8b3ca19927
Vladimir Kubantsev <trihlord@gmail.com>
2025-08-23 17:26:37 +0300

    chore(max_uniqe_split); impl dfs solution

--- max_uniqe_split/main.go
+++ max_uniqe_split/main.go
@@ -1,5 +1,25 @@
 package maxuniqesplit
 
 func MaxUniqeSplit(s string) int {
-	return 0
+	ss := map[string]struct{}{}
+	max := 0
+	var backtrace func(index int, cur int)
+	backtrace = func(index int, cur int) {
+		if index == len(s) {
+			if cur > max {
+				max = cur
+			}
+			return
+		}
+		for i := index; i < len(s); i++ {
+			sub := s[index : i+1]
+			if _, exists := ss[sub]; !exists {
+				ss[sub] = struct{}{}
+				backtrace(i+1, cur+1)
+				delete(ss, sub)
+			}
+		}
+	}
+	backtrace(0, 0)
+	return max
 }
